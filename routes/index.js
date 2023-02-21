deff781857ffebbac827bb18b0c62821445927a2
pankaj <pankajkatre722@gmail.com>
2023-02-21 19:04:53 +0530

    third

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/main', function(req, res, next) {
+  res.render('main');
 });
 
 // router.get('/', function(req, res, next) {
